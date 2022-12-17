1a8e84d94d982184d243bc47502df154ef9602cd
Lucas Ramos <xlucasx.blazz@hotmail.com>
2022-12-16 23:33:42 -0300

    add the instructions to connect fastapi with MySql

--- config/db.py
+++ config/db.py
@@ -3,5 +3,13 @@ from sqlalchemy import create_engine, MetaData
 
 meta = MetaData()
 
-engine = create_engine('mysql+pymysql://root:L42904361r.@localhost:3306/storedb')
+# Chanche the path following the next sequence to connect the 
+#   database with python
+# 
+# example:
+#      'mysql+pymysql://user:password.@IP:PORT/DATABASE
+path = 'mysql+pymysql://root:L42904361r.@localhost:3306/storedb'
+# note: the database has to be created before connecting to it
+
+engine = create_engine(path)
 conn = engine.connect()
