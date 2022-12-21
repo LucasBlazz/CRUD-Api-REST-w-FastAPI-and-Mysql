11dceaacbd361daa07122327aad413c90b2b422a
Lucas <58124822+LucasBlazz@users.noreply.github.com>
2022-12-21 20:11:46 -0300

    delete sensitive data

--- config/db.py
+++ config/db.py
@@ -3,12 +3,12 @@ from sqlalchemy import create_engine, MetaData
 
 meta = MetaData()
 
-# Chanche the path following the next sequence to connect the 
+# Chanche the path following the next format to connect the 
 #   database with python
 # 
 # example:
 #      'mysql+pymysql://user:password.@IP:PORT/DATABASE
-path = 'mysql+pymysql://root:L42904361r.@localhost:3306/storedb'
+path = ''
 # note: the database has to be created before connecting to it
 
 engine = create_engine(path)
